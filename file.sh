12ef707e94e616f090778d850f102baab4ef87c7
DevOps8883 <daradamidemi3@gmail.com>
2025-04-20 21:29:05 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -4,3 +4,13 @@ name="Jayesh"
 age=21
 echo $name $age
 echo "Adeleke David"
+
+num_1=13
+num_2=7
+
+ if [ "$num_1" -eq "$num_2" ]
+ then
+	echo "Statement is TRUE"
+ else
+	echo "Statement is FALSE"
+ fi
